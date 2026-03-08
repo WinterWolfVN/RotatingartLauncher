e1b13777a41fc3304e9cd86c2b5bc87cea85f9c7
WinterWolfVN <winterwolfvn123@gmail.com>
2026-03-08 16:52:36 +0700

    Delete shared/src/commonMain/kotlin/com/app/ralaunch/shared/core/util/HazeUtils.kt

--- shared/src/commonMain/kotlin/com/app/ralaunch/shared/core/util/HazeUtils.kt
+++
@@ -1,7 +0,0 @@
-package com.app.ralaunch.shared.core.util
-
-import androidx.compose.ui.Modifier
-import dev.chrisbanes.haze.HazeState
-
-// Expect function for safe Haze application
-expect fun Modifier.safeHaze(state: HazeState): Modifier
